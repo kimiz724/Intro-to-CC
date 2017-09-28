9e4051ae64462ededa0865f67abdf6ecd8c30a37
kimiz724 <32293571+kimiz724@users.noreply.github.com>
2017-09-28 17:34:06 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
+var cx = [];
+var cy = [];
+
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(200,200,0)//배경 색상
-frameRate(50); //1초에 몇 프레임이 이용되는지. 숫자가 낮을수록 천천히 원이 찍힌다.
+	size(600,600);
+	frameRate(6);
 }
+
 function draw() {
-	if (mouseIsPressed) {
-		var size = random(10, 100);
-		ellipse(mouseX, mouseY, size, size); } //mouseX,Y는 센터의 위치. 마우스를 클릭하면 그 위치를 중심으로 원 생성.
+	background(200);
+	fill(255);
+	noStroke();
+
+	for (var y =0;y<height; y+=10){
+	var snowX = random(0,width);
+	var snowSz = random(5,15);
+	ellipse(snowX, y, snowSz, snowSz);
+	}
 }
