25d2afb3e7653d775e717f06d2dea26a02e3aa0c
kimiz724 <32293571+kimiz724@users.noreply.github.com>
2017-12-16 13:18:24 +0900

    Update sketch_171021b.js

--- sketch_171021b.js
+++ sketch_171021b.js
@@ -1,12 +1,11 @@
-function setup() {
-  creatCanvas(windowWidth, windowHeight);
-  background(255,0,0);
+ function setup() { createCanvas(windowWidth, windowHeight); background(255, 0, 0);
 }
-
-function draw() {
-  for (var y=20; y<=80; y+=10) {
-    for (var x=20; x<=y; x+=5) {
-      line(x, y, x-3, y-3);
-    }
-  }
+function draw() { background(255, 0, 0); randomSeed(0); strokeWeight(3);
+var x, y;
+var delta = map(mouseX, 0, windowWidth, 10, 100);
+for (y=0; y<windowHeight; y+=delta) { for (x=0; x<windowWidth; x+=delta) {
+var r = random(0, 1); if (r < 0.5) {
+line(x, y, x+delta, y+delta); } else {
+line(x+delta, y, x, y+delta); }
+} }
 }
